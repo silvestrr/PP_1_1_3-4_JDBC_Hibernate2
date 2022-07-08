6bd65362d162c06dd755f7ca2b1be5c932d0e4c4
silvestrr <nikitamaliev000@gmail.com>
2022-07-08 18:06:27 +0300

    Revert "Delete src/main/java/jm/task/core/jdbc directory"
    This reverts commit 4179adae7e3b69db4620470175075942599df5d6.

---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,75 @@
+package jm.task.core.jdbc.util;
+
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+
+public class Util {
+    private static SessionFactory sessionFactory;
+    private Util() {}
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/user");
+                settings.put(Environment.USER, "root");
+                settings.put(Environment.PASS, "root");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+
+                settings.put(Environment.SHOW_SQL, "true");
+
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+                settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+                configuration.setProperties(settings);
+
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+
+
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        String URL = "jdbc:mysql://localhost:3306/user";
+        String LOGIN = "root";
+        String PASSWORD = "root";
+        return getConnection(URL,LOGIN,PASSWORD);
+    }
+
+
+    public static Connection getConnection(String URL, String LOGIN, String PASSWORD)  {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+            System.out.println("Connection to Store DB succesfull!");
+
+        } catch (SQLException e) {
+            System.out.println("Connection failed...");
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+
+}
+
